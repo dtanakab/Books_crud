f3a3d13c61efe6673e7420006fb094d4911944ec
dtanakab <karugamo.highkick@gmail.com>
2019-11-28 16:03:25 +0900

    Add github user authentication by using omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,11 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+<<<<<<< HEAD
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+=======
+  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+>>>>>>> 1f9a71e... Add pagination and arrange html
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -30,7 +34,11 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
+<<<<<<< HEAD
   require 'devise/orm/active_record'
+=======
+  require "devise/orm/active_record"
+>>>>>>> 1f9a71e... Add pagination and arrange html
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -297,5 +305,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :github, '9f979ce5c3be42e489c9', '5ee3f18b2c40ba8a3cb630c88d28855fec738003', scope: 'user,public_repo'
+  config.omniauth :github, "9f979ce5c3be42e489c9", "5ee3f18b2c40ba8a3cb630c88d28855fec738003", scope: "user,public_repo"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -92,4 +92,21 @@ ActiveRecord::Schema.define(version: 20_191_209_022_326) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.integer "postcode"
+    t.text "address"
+    t.text "introduction"
+    t.string "uid"
+    t.string "provider"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 end
