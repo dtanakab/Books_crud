630c568b905e2ee0b29b33339cd0ffd0a52f1846
dtanakab <karugamo.highkick@gmail.com>
2019-12-01 12:04:44 +0900

    Remove some dead code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,6 @@ class BooksController < ApplicationController
 
   # GET /books/1
   # GET /books/1.json
-  def show
-  end
 
   # GET /books/new
   def new
@@ -18,52 +16,31 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit
-  end
 
   # POST /books
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t('messages.created') }
-      else
-        format.html { render :new }
-      end
-    end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('messages.updated') }
-      else
-        format.html { render :edit }
-      end
-    end
-  end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t('messages.destroyed') }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title, :memo, :author, :picture)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title, :memo, :author, :picture)
+  end
 end
