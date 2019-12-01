17171e73d4ad1b84e21cbc3c0bb9767655837cdf
dtanakab <karugamo.highkick@gmail.com>
2019-12-01 12:23:00 +0900

    Remove some unused code and comments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,45 +1,45 @@
-class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /books
-  # GET /books.json
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
   def index
     @books = Book.all
   end
 
-  # GET /books/1
-  # GET /books/1.json
-
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
-
-  # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: t('messages.created')
+    else
+      render :new
+    end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
+  def update
+    @book.update(book_params)
+    if @book.update(book_params)
+      redirect_to @book, notice: t('messages.updated')
+    else
+      render :edit
+    end
+  end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
+    redirect_to books_url, notice: t('messages.destroyed')
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_book
     @book = Book.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def book_params
     params.require(:book).permit(:title, :memo, :author, :picture)
   end
