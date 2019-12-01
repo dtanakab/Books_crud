c293dd3ec5e119f1be48fc6ef7d95754515d8393
dtanakab <karugamo.highkick@gmail.com>
2019-12-01 17:11:26 +0900

    Remove unused files and codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+    def login_required
+      redirect_to new_user_session_path unless user_signed_in?
+    end
+
+  protected
+    def configure_permitted_parameters
+      added_attrs = [:username, :postcode, :address, :postcode, :introduction]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
+    end
+
+    def require_login
+      unless user_signed_in?
+        flash[:notice] = t("messages.login_needed")
+        redirect_to root_url
+      end
+    end
+
+    def identity_verification
+      unless User.find_by(id: params[:id]) == current_user
+        flash[:notice] = t("messages.userself_only")
+        redirect_to books_url
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 2019_11_28_082936) do
+=======
 ActiveRecord::Schema.define(version: 2019_11_27_074625) do
-
+>>>>>>> b60f7fd... Modify all files by rubocop
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -20,5 +25,23 @@ ActiveRecord::Schema.define(version: 2019_11_27_074625) do
     t.string "picture"
     t.string "author"
   end
+<<<<<<< HEAD
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.integer "postcode"
+    t.text "address"
+    t.text "introduction"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+=======
+>>>>>>> b60f7fd... Modify all files by rubocop
 end
