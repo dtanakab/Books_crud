c2ce7c267f1c4a1da43d554c5b9ee31d3c445c4f
dtanakab <karugamo.highkick@gmail.com>
2019-12-05 12:26:36 +0900

    Add user following

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
-    mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader
+  belongs_to :user
+  validates :title, presence: true
+  validates :memo, presence: true
 end
