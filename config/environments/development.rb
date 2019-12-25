b289ff470c6a44b9939e70f18e8eeec9a9719b34
dtanakab <karugamo.highkick@gmail.com>
2019-12-25 14:05:00 +0900

    modify .gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.webpacker.check_yarn_integrity = false
+  config.force_ssl = true
 end
