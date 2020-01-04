cdf947f8894f0e2806937d4d147e38d877057af1
dtanakab <karugamo.highkick@gmail.com>
2020-01-04 16:30:33 +0900

    add deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.11.2'
+
+set :application, 'book_app'
+
+set :repo_url, 'git@github.com:dtanakab/Books_crud.git'
+
+set :branch, 'test_production'
+
+set :deploy_to, '/opt/book_app'
+
+set :rbenv_ruby, '2.6.5'
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_files, 'config/master.key'
